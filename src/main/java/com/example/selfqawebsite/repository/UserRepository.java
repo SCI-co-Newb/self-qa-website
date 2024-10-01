a1a9031b7a2fc80bc4e34db509b40b0e8ad96e82
Sahajdeep Singh <impulseVD@hotmail.com>
2024-09-30 23:23:03 -0400

    Stopped at 15:42 on "Spring Boot, MySQL, JPA, Hibernate..." video

--- src/main/java/com/example/selfqawebsite/repository/UserRepository.java
+++ src/main/java/com/example/selfqawebsite/repository/UserRepository.java
@@ -3,5 +3,11 @@ package com.example.selfqawebsite.repository;
 import com.example.selfqawebsite.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByUsernameAndPassword(String username, String password);
+
+    void deleteByUsernameAndPassword(String username, String password);
+}
 
--- src/main/java/com/example/selfqawebsite/service/UserService.java
+++ src/main/java/com/example/selfqawebsite/service/UserService.java
@@ -1,4 +1,42 @@
 package com.example.selfqawebsite.service;
 
+import com.example.selfqawebsite.entity.User;
+import com.example.selfqawebsite.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // POST method
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    // GET methods
+    public User findById(long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User findByUsernameAndPassword(String username, String password) {
+        return userRepository.findByUsernameAndPassword(username, password);
+    }
+
+    // DELETE method
+    public String deleteById(long id) {
+        userRepository.deleteById(id);
+        return "product removed";
+    }
+
+    public String deleteByUsernameAndPassword(String username, String password) {
+        userRepository.deleteByUsernameAndPassword(username, password);
+        return "product removed";
+    }
 }
