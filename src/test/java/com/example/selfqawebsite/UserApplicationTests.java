8e48e8fb38bd4a9de22360003cc730c6cf40978f
Sahajdeep Singh <impulseVD@hotmail.com>
2024-10-01 15:16:20 -0400

    userCreated test works

--- src/test/java/com/example/selfqawebsite/UserApplicationTests.java
+++ src/test/java/com/example/selfqawebsite/UserApplicationTests.java
@@ -9,10 +9,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = SelfQaWebsiteApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UserApplicationTests {
+
     @Autowired
     TestRestTemplate restTemplate;
 
@@ -21,9 +24,13 @@ class UserApplicationTests {
         User newUser = new User();
         newUser.setUsername("admin");
         newUser.setPassword("admin");
-        ResponseEntity<User> responseEntity = restTemplate.postForEntity("/users", newUser, User.class);
+        ResponseEntity<Void> responseEntity = restTemplate.postForEntity("/users", newUser, Void.class);
 
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        URI location = responseEntity.getHeaders().getLocation();
+        assertThat(location).isNotNull();
+        assertThat(location.getPath()).startsWith("/users/");
     }
 
 }
